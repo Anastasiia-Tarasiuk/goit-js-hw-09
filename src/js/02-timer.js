56bc271c94a802a208a2f5f9e93b148de0d665d1
Anastasiia-Tarasiuk <a.yu.tarasiuk@gmail.com>
2022-05-13 21:51:11 +0300

    Починає завдання

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,50 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+let timer = null;
+
+const inputEl = document.querySelector('#datetime-picker');
+const startBtnEl = document.querySelector('button');
+const spanDaysEl = document.querySelector('[data-days]');
+const spanHoursEl = document.querySelector('[data-hours]');
+const spanMinutesEl = document.querySelector('[data-minutes]');
+const spanSecondsEl = document.querySelector('[data-seconds]');
+
+startBtnEl.disabled = true;
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+        // console.log(selectedDates[0]);
+
+        // if (selectedDates[0] < this.defaultDate) ЧОМУ this не працює???
+        if (selectedDates[0] < options.defaultDate) {
+            alert("Please choose a date in the future");
+        } else {
+            startBtnEl.disabled = false;
+        }
+
+        let deltaTime = selectedDates[0] - options.defaultDate;
+
+        const convertedTime = convertMs(deltaTime);
+
+        console.log(convertedTime);
+
+        const { days, hours, minutes, seconds } = convertedTime;
+
+        spanDaysEl.textContent = days;
+        spanHoursEl.textContent = hours;
+        spanMinutesEl.textContent = minutes;
+        spanSecondsEl.textContent = seconds;
+
     },
 };
 
-console.log(options.defaultDate)
 
-const inputEl = document.querySelector('#datetime-picker');
-const startBtnEl = document.querySelector('button');
-startBtnEl.disabled = true;
+
 
 startBtnEl.addEventListener('click', onStartBtnClick);
 
@@ -23,11 +52,38 @@ flatpickr(inputEl, options);
 
 
 function onStartBtnClick() {
-
-    // if (selectedDates > )
-
-    console.log("hi");
+    timer = setInterval(() => {
+        console.log("hi");
+       
+        console.log(options.onClose());
+    }, 1000);
 }
 
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+   return String(value).padStart(2, '0');
+}
 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+const inputDelayEl = document.querySelector('[name="delay"]');
+const inputStepEl = document.querySelector('[name="step"]');
+const inputAmountEl = document.querySelector('[name="amount"]');
+// const btnEl = document.querySelector('button');
+const formEl = document.querySelector('form');
+let timerId = null;
+
+
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  
+  let counter = 0;
+  
+  
+
+    timerId = setInterval(() => {
+      counter += 1;
+      if (counter >= inputAmountEl.value) {
+        clearInterval(timerId);
+      }
+      createPromise("0", inputDelayEl.value);
+    }, inputDelayEl.value);
+  }
+ 
+
+
+
+
 function createPromise(position, delay) {
+
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
